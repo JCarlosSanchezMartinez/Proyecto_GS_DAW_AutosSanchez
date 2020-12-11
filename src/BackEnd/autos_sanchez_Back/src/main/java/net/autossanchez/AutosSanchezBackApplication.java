69885651d6e7b83d2252071521c1371dd13083de
JuanCarlosSM <Alum.jcarlossanchezm@iesalixar.com>
2020-12-12 00:45:09 +0100

    prueba des

--- src/BackEnd/autos_sanchez_Back/src/main/java/net/autossanchez/AutosSanchezBackApplication.java
+++ src/BackEnd/autos_sanchez_Back/src/main/java/net/autossanchez/AutosSanchezBackApplication.java
@@ -2,12 +2,19 @@ package net.autossanchez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutosSanchezBackApplication {
+public class AutosSanchezBackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutosSanchezBackApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutosSanchezBackApplication.class);
 	}
 
 }
--- src/FrontEnd/src/app/components/gallery-management/gallery-management.component.ts
+++ src/FrontEnd/src/app/components/gallery-management/gallery-management.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SortEvent } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { SearchVehicleDto } from 'src/app/model/search-vehicle-dto';
 import { Vehicle } from 'src/app/model/Vehicle';
@@ -16,7 +17,8 @@ import { GalleryVehicleComponent } from './gallery-vehicle/gallery-vehicle.compo
   providers: [DialogService]
 })
 export class GalleryManagementComponent implements OnInit {
-  
+
+  public columnsTableResult: any[]; // array de columnas
   showTable: boolean;
   vehiclesDto:SearchVehicleDto[];
   public visibleSidebar= false;
@@ -27,7 +29,20 @@ export class GalleryManagementComponent implements OnInit {
      private seriveToken: TokenService,
      private common: CommonModule,
      private router: Router,
-     public dialogService: DialogService,) { }
+     public dialogService: DialogService,) {
+      this.columnsTableResult = [
+
+        { field: 'brand', header: 'Marca' },
+        { field: 'model', header: 'Modelo' },
+        { field: 'imagen', header: 'Imagen' },
+        { field: 'kms', header: 'Kilometros' },
+        { field: 'fuel', header: 'Combustible' },
+        { field: 'sellDate', header: 'Matriculacion' },
+        { field: 'price', header: 'Precio' },
+
+  
+      ];
+      }
 
   
   ngOnInit() {
@@ -59,9 +74,30 @@ export class GalleryManagementComponent implements OnInit {
   }
 
 
-  gallery(){
+  showTableOrCard(){
     this.showTable = true;
   }
+    // ORDENACION DE COLUMNAS
+    customSort(event: SortEvent) {
+      event.data.sort((data1, data2) => {
+        let value1 = data1[event.field];
+        let value2 = data2[event.field];
+        let result = null;
+  
+        if (value1 == null && value2 != null)
+          result = -1;
+        else if (value1 != null && value2 == null)
+          result = 1;
+        else if (value1 == null && value2 == null)
+          result = 0;
+        else if (typeof value1 === 'string' && typeof value2 === 'string')
+          result = value1.localeCompare(value2);
+        else
+          result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+  
+        return (event.order * result);
+      });
+    }
 
 
 
